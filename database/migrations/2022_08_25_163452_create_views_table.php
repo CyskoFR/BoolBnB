c4f4ba45317c5bc58548747662b36326ff95a3dd
RubaCodes <rubacodes@gmail.com>
2022-08-25 18:39:04 +0200

    creata tab: views

---
+++ database/migrations/2022_08_25_163452_create_views_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->ipAddress('ip_visitor');
+            $table->foreignId('apartment_id')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+}
