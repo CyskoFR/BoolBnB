73021c0c9fcdbe0421fba13b0fc944a6b4f890cd
RubaCodes <rubacodes@gmail.com>
2022-08-25 17:43:58 +0200

    create migrations categories e apartments **bug**

---
+++ database/migrations/2022_08_25_144712_create_apartments_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            //fk users
+             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            // fk categories **** da fixare cascade => set null ****
+             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade')->nullable();
+            $table->string('name');
+            $table->tinyInteger('rooms')->unsigned(); // da 0 a 255
+            $table->tinyInteger('beds')->unsigned();
+            $table->tinyInteger('bathrooms')->unsigned();
+            $table->text('description');
+            $table->smallInteger('size')->unsigned(); // fino a 65k m2
+            $table->string('full_address');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->string('image');
+            $table->boolean('is_visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('apartments', function(Blueprint $table){
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropIfExists('apartments');
+        });
+    }
+}
