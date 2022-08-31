9f6ae83387534e91f9000ff733f2e2001616a714
Fabio Moro <fabio.moro2000@gmail.com>
2022-08-31 11:15:47 +0200

    ulteriore stile index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
 <section id="index-apartment" class="p-3">
-    <div class="container text-capitalize " id="index">
-        <h1 class="text-center mb-2 text-light">Benvenuto {{ Auth::user()->first_name }}</h1>
+    <div class="container text-capitalize d-flex flex-column" id="index">
+        <h1 class="text-center mt-3 mb-3 text-light">Benvenuto {{ Auth::user()->first_name }}</h1>
+        
+        <a class="align-self-center" href="{{route('admin.apartments.create')}}">
+            <button class="btn mb-4"><b>Crea un nuovo appartamento</b></button>
+        </a>
+
         @foreach ($apartments as $apartment)
         <div class="apartment mb-3 p-3 d-flex justify-content-between align-items-center">
             <div>
@@ -15,18 +20,18 @@
             </div>
             <div class="buttons">
                 <a href="">
-                    <button class="btn">Messaggi</button>
+                    <button class="btn"><b>Messaggi</b></button>
                 </a>
                 
                 <a href="{{route('admin.apartments.edit', $apartment)}}">
-                    <button class="btn">Modifica</button>
+                    <button class="btn"><b>Modifica</b></button>
                 </a>
             </div>
         </div>
         @endforeach
 
-        <a href="{{route('admin.dashboard')}}">
-            <button class="btn">Home</button>
+        <a href="/">
+            <button class="btn"><b>Home</b></button>
         </a>
     </div>
 </section>
