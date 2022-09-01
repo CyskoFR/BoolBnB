be68b63933f061f2237e1d165a727549a5cc8089
olic96 <francoolich@gmail.com>
2022-09-01 17:48:31 +0200

    fix index sponsorships and show

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -3,17 +3,34 @@
 @section('content')
 <section id="index-sponsorships" class="p-5">
     <div class="container d-flex flex-column align-items-center text-center">
-        <div class="sponsorship twentyfour mt-3 py-4">
-            <a href="#">Upgrade di 24 ore</a>
-            <div>2,99 €</div>
+        <div class="sponsorship gold d-flex justify-content-around mt-3 py-4">
+            <div class="rombo">
+                <div class="label">Gold</div>
+            </div>
+            <div>
+                <div>Upgrade di 144 ore!</div>
+
+                <div>9,99 €</div>
+            </div>
         </div>
-        <div class="sponsorship seventytwo mt-3 mb-3 py-4">
-            <a href="#">Upgrade di 72 ore</a>
-            <div>5,99 €</div>
+        <div class="sponsorship silver d-flex justify-content-around mt-3 py-4">
+            <div>
+                <div>Upgrade di 72 ore!</div>
+
+                <div>5,99 €</div>
+            </div>
+            <div class="rombo">
+                <div class="label">Silver</div>
+            </div>
         </div>
-        <div class="sponsorship onehundredfortyfour mb-3 py-4">
-            <a href="#">Upgrade di 144 ore</a> 
-            <div>9,99 €</div>
+        <div class="sponsorship bronze d-flex justify-content-around mt-3 py-4">
+            <div class="rombo">
+                <div class="label_bronze">Bronze</div>
+            </div>
+            <div>
+                <div>Upgrade di 24 ore!</div>
+                <div>2,99 €</div>
+            </div>
         </div>
     </div>
 </section>
