d03ea22d5c0939b506a7c1407a28895fbcdb04ed
AlessP93 <alespiscino@libero.it>
2022-09-02 18:19:24 +0200

    pagina login work in progress

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -28,7 +28,6 @@
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon">ciao ciao ciao</span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
