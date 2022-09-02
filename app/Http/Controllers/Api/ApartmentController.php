c94320f8a9b64820faf8b63775ce917e3b0d5f09
RubaCodes <rubacodes@gmail.com>
2022-09-02 12:01:16 +0200

    aggiunta rotta apartment/search + validazione $get

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -10,13 +10,37 @@ use App\Service;
 class ApartmentController extends Controller
 {
     
-
+    //lista completa appartamenti
     public function index()
     {
         $apartments = Apartment::where('is_visible', true)->with('user', 'category', 'services')->get();
 
         return $apartments;
     }
+        /**
+     * @param int $id
+     *
+     *
+     */
+    //Singolo appartemento
+    public function searchApartment(Request $request)
+    {
+        $request->validate([
+            'full_address' => 'required|string|max:255',
+            'rooms' => 'integer|min:1|max:255',
+            'beds' => 'integer|min:1|max:255',
+            'category_id' => 'exists:categories,id',
+            'services' => 'sometimes|exists:services,id'
+        ]);
+
+        $request->all();
+
+
+        $apartment = Apartment::all();
+
+        return $apartment;
+    }
+
 
     /**
      * @param int $id
