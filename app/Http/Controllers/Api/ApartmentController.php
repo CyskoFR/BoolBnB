47737d0d7ec3b98d594a3f01b3ebb96f9136d836
RubaCodes <rubacodes@gmail.com>
2022-09-02 17:38:37 +0200

    calcolo mat lat e lon baoudries riuscito

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Apartment;
 use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Builder;
+use Mockery\Undefined;
 
 class ApartmentController extends Controller
 {
@@ -39,59 +40,41 @@ class ApartmentController extends Controller
         ]);
         //fetch dei dati
         $data = $request->all();
-        // //geolocalizzazione indirizzo
-        // $rawAddress = $data['full_address'];
-        // $geoResponse = Http::get("https://api.tomtom.com/search/2/search/{$rawAddress}.json", [
-        //     'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
-        // ])->json();
-
-        // //cast float to string 
-        // $final_address = $geoResponse['results']['0']['position'];
-        // $final_address_lat = number_format($final_address['lat'],5,'.','');
-        // $final_address_lon = number_format($final_address['lon'],5,'.','');
-        // //dd( $final_address_lat, $final_address_lon);
-
-        //
-        // $geometry_list =
-        // [
-        //     "type" => 'CIRCLE',
-        //     "position" => "{$final_address_lat},{$final_address_lon}",
-        //     "radius" => "20000" 
-        // ];
-        // $test = implode("",$geometry_list);
-
-        // //dd($test);
-        // $result = Http::post("https://api.tomtom.com/search/2/geometrySearch/{$rawAddress}.json?", [
-        //     'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
-        //      'geometryList' => $geometry_list,
-        //     ])->json();
-
-        // $client = new Client([
-        //     'headers' => ['Content-Type' => 'application/json']
-        //     ]
-        // );
-
-        // $response = $client->get("https://api.tomtom.com/search/2/geometrySearch/{$rawAddress}.json?key=RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2", [
-        //     // 'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
-        //     "json" =>['geometryList'=> $test],
-        // ]);
-        //dd($result);
-
+        //geolocalizzazione indirizzo
+        $rawAddress = $data['full_address'];
+        $geoResponse = Http::get("https://api.tomtom.com/search/2/search/{$rawAddress}.json", [
+            'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
+        ])->json();
+        //cast float to string 
+        $final_address = $geoResponse['results']['0']['position'];
+        $final_address_lat = $final_address['lat'];
+        $final_address_lon = $final_address['lon'];
+        //dd( $final_address_lat, $final_address_lon);
+        $boundries = $this->fetchBoundries($final_address_lat, $final_address_lon, 10);   
+        dd($boundries);
+        if(!isset($data['category_id'])){
         $apartments = Apartment::query()
-        ->where('rooms','>=', $data['rooms'] ?? 1)
-        ->orWhere('category_id',"=", $data['category_id'] )
-        ->orWhere('beds','>=', $data['beds'] ?? 1)
-
-
-        // ->orWhereHas(function ($query){
-        //     $query->select('apartment_id')
-        //     ->from('service')
-        //     ->whereColumn('service_id', 'service.id');
-        // })
-        //->with('user', 'category', 'services')
+        ->where([
+            ['rooms','>=', $data['rooms'] ?? 1],
+            ['beds','>=', $data['beds'] ?? 1],
+            []
+            ])
+            //->with('category', 'user', 'services')
         ->get();
-
         return $apartments;
+        }else{
+            $apartments = Apartment::query()
+            ->where([
+                ['rooms','>=', $data['rooms'] ?? 1],
+                ['beds','>=', $data['beds'] ?? 1],
+                ['category_id'] , $data['category_id']                
+                ])
+                //->with('category', 'user', 'services')
+            ->get();
+            return $apartments;
+
+        }
+        
     }
 
 
@@ -103,9 +86,39 @@ class ApartmentController extends Controller
     //Singolo appartemento
     public function getApartment(Request $request)
     {
+        $this->hello();
         $id = $request->all();
         $apartment = Apartment::query()->where('id', $id)->with('user', 'category', 'services')->get();
 
         return $apartment;
+    }
+
+    // thanks to http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates#Longitude
+    public function fetchBoundries(float $lat_deg, float $lon_deg , int $distance ){
+        $EARTHS_RADIUS_KM = 6371.01;
+        //conversione deg / rad
+        $lat_rad = $lat_deg*(pi()/180);
+        $log_rad = $lon_deg*(pi()/180);
+        //raggio angolare
+        $angular_radius = $distance / $EARTHS_RADIUS_KM;
+        //min max lat in rad
+        $lat_min_rad = $lat_rad - $angular_radius; 
+        $lat_max_rad = $lat_rad + $angular_radius; 
+        //delta lon
+        $deltaLon = asin(sin($angular_radius) / cos($lat_rad));
+        //min max lon in rad
+        $lon_min_rad = $log_rad - $deltaLon;
+        $lon_max_rad = $log_rad + $deltaLon;
+
+        //min max lat in deg
+        $lat_min_deg = $lat_min_rad * (180/pi());
+        $lat_max_deg = $lat_max_rad * (180/pi());
+        //min max log in deg
+        $lon_min_deg = $lon_min_rad * (180/pi());
+        $lon_max_deg = $lon_max_rad * (180/pi());
+        return [
+            'lat' => [$lat_min_deg ,$lat_max_deg],
+            'log' => [$lon_min_deg, $lon_max_deg]
+        ];
     }
 }
