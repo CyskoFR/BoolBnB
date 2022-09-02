fdaeebd8e87f82cc197e0641509da4c694d7ce1e
RubaCodes <rubacodes@gmail.com>
2022-09-02 15:48:22 +0200

    non ce sto a capi un cazzo

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 //models
 use App\Apartment;
+use GuzzleHttp\Client;
+use Illuminate\Database\Eloquent\Builder;
 
 class ApartmentController extends Controller
 {
@@ -37,31 +39,56 @@ class ApartmentController extends Controller
         ]);
         //fetch dei dati
         $data = $request->all();
-        //geolocalizzazione indirizzo
-        $rawAddress = $data['full_address'];
-        $geoResponse = Http::get("https://api.tomtom.com/search/2/search/{$rawAddress}.json", [
-            'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
-        ])->json();
+        // //geolocalizzazione indirizzo
+        // $rawAddress = $data['full_address'];
+        // $geoResponse = Http::get("https://api.tomtom.com/search/2/search/{$rawAddress}.json", [
+        //     'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
+        // ])->json();
 
-        //cast float to string 
-        $final_address = $geoResponse['results']['0']['position'];
-        $final_address_lat = number_format($final_address['lat'],5,'.','');
-        $final_address_lon = number_format($final_address['lon'],5,'.','');
+        // //cast float to string 
+        // $final_address = $geoResponse['results']['0']['position'];
+        // $final_address_lat = number_format($final_address['lat'],5,'.','');
+        // $final_address_lon = number_format($final_address['lon'],5,'.','');
+        // //dd( $final_address_lat, $final_address_lon);
 
         //
-        $geometry_list="type=CIRCLE,position={$final_address_lat},{$final_address_lon},radius=20000" ;
-     
-        //dd($geometry_list);
-        $result = Http::get("https://api.tomtom.com/search/2/geometrySearch/.json?", [
-            'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
-            'geometryList' => "{$geometry_list}",
-            ]);
-        dd($result);
+        // $geometry_list =
+        // [
+        //     "type" => 'CIRCLE',
+        //     "position" => "{$final_address_lat},{$final_address_lon}",
+        //     "radius" => "20000" 
+        // ];
+        // $test = implode("",$geometry_list);
+
+        // //dd($test);
+        // $result = Http::post("https://api.tomtom.com/search/2/geometrySearch/{$rawAddress}.json?", [
+        //     'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
+        //      'geometryList' => $geometry_list,
+        //     ])->json();
+
+        // $client = new Client([
+        //     'headers' => ['Content-Type' => 'application/json']
+        //     ]
+        // );
+
+        // $response = $client->get("https://api.tomtom.com/search/2/geometrySearch/{$rawAddress}.json?key=RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2", [
+        //     // 'key' => 'RYIXIrvLjWrNeQyGjLi5JoEGgH0IPDU2',
+        //     "json" =>['geometryList'=> $test],
+        // ]);
+        //dd($result);
+
         $apartments = Apartment::query()
-        ->where('rooms', $data['rooms'])
-        ->orWhere('category_id', $data['category_id'])
-        ->orWhere('beds', $data['beds'])
-        ->with('user', 'category', 'services')
+        ->where('rooms','>=', $data['rooms'] ?? 1)
+        ->orWhere('category_id',"=", $data['category_id'] )
+        ->orWhere('beds','>=', $data['beds'] ?? 1)
+
+
+        // ->orWhereHas(function ($query){
+        //     $query->select('apartment_id')
+        //     ->from('service')
+        //     ->whereColumn('service_id', 'service.id');
+        // })
+        //->with('user', 'category', 'services')
         ->get();
 
         return $apartments;
