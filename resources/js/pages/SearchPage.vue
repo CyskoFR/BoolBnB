e927a5f5f3e9f1b034469a8f42c2312a508f71d6
RubaCodes <rubacodes@gmail.com>
2022-09-05 14:56:05 +0200

    riorganizzazione di header e footer

--- resources/js/pages/SearchPage.vue
+++ resources/js/pages/SearchPage.vue
@@ -1,50 +1,46 @@
 <template>
     <div>
-        <HeaderComponent/>
-        <bnbCard :apartment="apartment" v-for="apartment in apartments" :key="apartment.id"/>
-        <FooterComponent/>
+        <bnbCard
+            :apartment="apartment"
+            v-for="apartment in apartments"
+            :key="apartment.id"
+        />
     </div>
 </template>
 
 <script>
-
-import HeaderComponent from './components/HeaderComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
-import bnbCard from './microComponents/bnbCard.vue';
+import bnbCard from "./microComponents/bnbCard.vue";
 
 export default {
     name: "SearchPage",
-    components: { HeaderComponent, FooterComponent, bnbCard },
+    components: { bnbCard },
     data() {
-
         return {
             apartments: [],
-            param: this.$route.params.param, 
-        }
+            param: this.$route.params.param,
+        };
     },
 
     created() {
-        
-        axios.get('/api/apartments/search', {
-            params: {
-                'full_address': this.param,
-                'rooms': '',
-                'beds': '',
-                'category_id': '',
-                'services': '',
-                'distance': '',
-            }
-        })
+        axios
+            .get("/api/apartments/search", {
+                params: {
+                    full_address: this.param,
+                    rooms: null,
+                    beds: null,
+                    category_id: null,
+                    services: null,
+                    distance: null,
+                },
+            })
 
-        .then((response) => {
-            this.apartments = response.data;
-        });
-    }
-
+            .then((response) => {
+                this.apartments = response.data;
+            });
+    },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../../sass/variables';
-    
+@import "../../sass/variables";
 </style>
