bf30d9db508341851a9f66442b3435e6e31217fa
RubaCodes <rubacodes@gmail.com>
2022-09-06 12:51:13 +0200

    aggiunto filtro servizi

--- resources/js/pages/sections/filterSection.vue
+++ resources/js/pages/sections/filterSection.vue
@@ -28,6 +28,84 @@
                     min="1"
                     step="1"
                 />
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-toggle="modal"
+                    data-target="#exampleModalCenter"
+                >
+                    servizi
+                </button>
+
+                <!-- Modal -->
+                <div
+                    class="modal fade"
+                    id="exampleModalCenter"
+                    tabindex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalCenterTitle"
+                    aria-hidden="true"
+                >
+                    <div
+                        class="modal-dialog modal-dialog-centered"
+                        role="document"
+                    >
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5
+                                    class="modal-title"
+                                    id="exampleModalLongTitle"
+                                >
+                                    Seleziona i servizi che ritieni
+                                    indispensabili
+                                </h5>
+                                <button
+                                    type="button"
+                                    class="close"
+                                    data-dismiss="modal"
+                                    aria-label="Close"
+                                >
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div
+                                        @click="selectService(service.id)"
+                                        v-for="service in allServices"
+                                        :key="service.id"
+                                        class="service col-3"
+                                    >
+                                        <input
+                                            type="checkbox"
+                                            :name="service.id"
+                                            :id="service.id"
+                                        />
+                                        <label :for="service.id">
+                                            {{ service.name }}</label
+                                        >
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button
+                                    type="button"
+                                    class="btn btn-secondary"
+                                    data-dismiss="modal"
+                                >
+                                    Chiudi
+                                </button>
+                                <button
+                                    @click="save()"
+                                    type="button"
+                                    class="btn btn-primary"
+                                >
+                                    Salva
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <button>submit</button>
             </form>
         </div>
@@ -41,6 +119,9 @@ export default {
     data() {
         return {
             observable,
+            selectedServicesString: "",
+            allServices: {},
+            tempServicesArray: new Set(),
         };
     },
     methods: {
@@ -53,6 +134,7 @@ export default {
                         beds: observable.beds,
                         distance: observable.distance,
                         category_id: observable.category_id,
+                        services: this.selectedServicesString,
                     },
                 })
                 .then((response) => {
@@ -63,6 +145,27 @@ export default {
                     console.log(error);
                 });
         },
+        selectService(id) {
+            this.tempServicesArray.has(id)
+                ? this.tempServicesArray.delete(id)
+                : this.tempServicesArray.add(id);
+        },
+        save() {
+            observable.selectedServices = Array.from(this.tempServicesArray);
+            this.selectedServicesString =
+                observable.selectedServices.toString();
+        },
+    },
+    created() {
+        axios
+            .get("/api/services")
+            .then((response) => {
+                console.log(response);
+                this.allServices = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     },
 };
 </script>
