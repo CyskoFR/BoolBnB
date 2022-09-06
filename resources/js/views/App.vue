6ec4626a161b4f52f7f14708b0bfb7b59fe0d904
CyskoFR <franc.sko91@gmail.com>
2022-09-06 18:54:35 +0200

    fix pagina 404

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="app">
         <header-component></header-component>
         <router-view></router-view>
         <footer-component></footer-component>
@@ -7,10 +7,25 @@
 </template>
 
 <script>
+
 import FooterComponent from "../pages/components/FooterComponent.vue";
 import HeaderComponent from "../pages/components/HeaderComponent.vue";
+
 export default {
+
     name: "App",
-    components: { FooterComponent, HeaderComponent },
+
+    components: { 
+        FooterComponent, 
+        HeaderComponent,
+    },
 };
 </script>
+
+<style lang="scss" scoped>
+    #app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+</style>
