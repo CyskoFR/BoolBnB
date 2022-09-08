f6520c00ed6b85dab39b2708f548bff195c69883
CyskoFR <franc.sko91@gmail.com>
2022-09-08 16:41:27 +0200

    aggiunto stile index + show messaggi

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,15 +1,42 @@
 @extends('layouts.back')
 
 @section('content')
-<section id="show-messages" class="p-3">
-
-    {{($message->apartment->user->email)}}
-    {{$message->text}}
-
-    <form action="{{ route('admin.message.destroy', [$message->apartment ,$message]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button onclick="return confirm('Sei sicuro di voler eliminare questo messaggio?')" class="btn btn-danger">Elimina Messaggio</button>
-    </form>
+<section id="show-messages" class="p-4 pt-5">
+    <div class="message_container mb-4 p-3">
+        <div class="owner_info_section">
+            <h3>Messaggio su: <b>{{$message->apartment->name}}</b></h3>
+            <h5>in: <b>{{$message->apartment->full_address}}</b></h5>
+            <p>del {{date('d-M-y h:i', strtotime($message->created_at))}}</p>
+        </div>
+        <div class="sender_info_section p-3 pb-4">
+            <h4><b>{{$message->full_name}}</b> scrive:</h4>
+            <p>{{$message->text}}</p>
+        </div>
+    </div>
+    <button class="btn btn-danger"data-toggle="modal" data-target="#deleteButton">Elimina Messaggio</button>
+    <!-- Delete Confirm Modal -->
+    <div class="modal fade" id="deleteButton" tabindex="-1" role="dialog" aria-labelledby="deleteButtonLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-center ">
+                    <b>Sei sicuro di voler eliminare questo messaggio?</b>
+                </div>
+                <div class="modal-footer">
+                    {{-- form per il destroy --}}
+                    <form class="mx-2" action="{{ route('admin.message.destroy', [$message->apartment ,$message]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger"data-toggle="modal" data-target="#deleteButton">Elimina</button>
+                    </form>            
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
