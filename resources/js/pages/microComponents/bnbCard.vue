2fff162a080682a28a9262851eaf7d92946f4ef7
RubaCodes <rubacodes@gmail.com>
2022-09-09 18:25:44 +0200

    aggiunta stellina sponsorizzati

--- resources/js/pages/microComponents/bnbCard.vue
+++ resources/js/pages/microComponents/bnbCard.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="apartment-card">
         <div class="img-container">
+            <div v-if="apartment.expiration_date">
+                <font-awesome-icon icon="fa-solid fa-star " />
+                <font-awesome-icon icon="fa-solid fa-star " id="up" />
+            </div>
+
             <a :href="`/bnb-view/data/${apartment.id}`">
-            <img class="apartment_img" :src="`/storage/${apartment.image}`" :alt="apartment.name">
+                <img
+                    class="apartment_img"
+                    :src="`/storage/${apartment.image}`"
+                    :alt="apartment.name"
+                />
             </a>
         </div>
-        <h5 class="apartment_name">{{apartment.name}}</h5>
-        <p class="apartment_address">{{apartment.full_address}}</p>
+        <h5 class="apartment_name">
+            {{ apartment.name }}
+        </h5>
+        <p class="apartment_address">{{ apartment.full_address }}</p>
     </div>
 </template>
 
@@ -18,27 +29,38 @@ export default {
         apartment: Object,
     },
 };
-
 </script>
 
 <style lang="scss" scoped>
-@import '../../../sass/variables';
+@import "../../../sass/variables";
 
 .apartment-card {
-    margin: .75rem;
+    margin: 0.75rem;
     transition: 0.1s;
     &:hover {
         scale: 102%;
     }
-    
+
     .img-container {
+        position: relative;
         width: 300px;
         height: 300px;
         border-radius: 20px;
         background-color: $bg-primary-light;
         border: 1px solid $primary-green;
         overflow: hidden;
-
+        .fa-star {
+            font-size: 1.5rem;
+            color: rgba(0, 0, 0, 0.445);
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            scale: 1.3;
+        }
+        #up {
+            color: rgb(255, 196, 0);
+            scale: 1.1;
+        }
         img {
             max-width: 100%;
             height: 100%;
@@ -49,7 +71,7 @@ export default {
     .apartment_name {
         color: $text-gray-light;
         font-size: 1.5rem;
-        margin: .5rem 0 0;
+        margin: 0.5rem 0 0;
     }
     .apartment_address {
         color: $text-gray-dark;
