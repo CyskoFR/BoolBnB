21007c7371d39baa320393ff07ac5c96121b051c
olic96 <francoolich@gmail.com>
2022-09-09 16:28:04 +0200

    add transiction page, fix route & btns

--- resources/views/admin/sponsorships/transactionResult.blade.php
+++ resources/views/admin/sponsorships/transactionResult.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.back')
 
 @section('content')
-@dd($transaction);
+
+<section id="transaction-result" class="d-flex flex-column align-items-center py-5">
+    <h2>Transasione di {{$transaction->amount}} eseguita con successo!</h2>
+    <a href="{{route('admin.apartments.index')}}">
+        <button class="btn update_button my-4">Torna ai tuoi appartamenti</button>
+    </a>
+</section>
+
 @endsection
