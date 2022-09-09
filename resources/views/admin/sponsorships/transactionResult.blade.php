b509417b86baa41136535b4a664038e41637c224
olic96 <francoolich@gmail.com>
2022-09-09 16:45:35 +0200

    fix transiction page

--- resources/views/admin/sponsorships/transactionResult.blade.php
+++ resources/views/admin/sponsorships/transactionResult.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.back')
 
 @section('content')
-
+{{-- @dd($transaction); --}}
 <section id="transaction-result" class="d-flex flex-column align-items-center py-5">
-    <h2>Transasione di {{$transaction->amount}} eseguita con successo!</h2>
+    <h2>La tua transasione di {{$transaction->amount}}€ è stata eseguita con successo!</h2>
+    <h3>Grazie per aver effettuato l'upgrade {{$transaction->customer['firstName']}}!</h3>
     <a href="{{route('admin.apartments.index')}}">
         <button class="btn update_button my-4">Torna ai tuoi appartamenti</button>
     </a>
